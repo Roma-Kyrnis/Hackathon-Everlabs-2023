4a8efdacbf2cb3f1962be8f12dc38dc8a912b8df
Roma-Kyrnis <kirnos007@gmail.com>
2023-11-05 03:57:38 +0200

    feat: add sender and receiver

--- Backend/server.js
+++ Backend/server.js
@@ -20,45 +20,21 @@ const USERS = [
   },
 ];
 
-const isUserExist = (id) => USERS.find((user) => user.id === id);
-
-const sendEmail = (email, message) => {
-  console.log(`Message '${message}' sent to '${email}'`);
+const sendToEmail = (senderEmail, receiverEmail, message) => {
+  console.log(`Message '${message}' sent to '${senderEmail}' from '${receiverEmail}'`);
 };
 
-const sendMessages = (users, message) => {
-  for (const user of users) {
-    sendEmail(user.email, message);
-  }
+const sendMessage = (senderUserId, receiverUserId, message) => {
+  const senderEmail = USERS.find((user) => user.id === senderUserId).email;
+  const receiverEmail = USERS.find((user) => user.id === receiverUserId).email;
+  sendToEmail(senderEmail, receiverEmail, message);
 };
 
 wss.on("connection", (ws, req) => {
   console.log("Connected");
-  let owner_id;
+  ws.send(JSON.stringify({ users: USERS }));
 
-  /**
-   * Expect:
-   * 1. Error if no ID in data field or local variable 'id'
-   * 2. If ID exist in data field and 'id' is empty
-   * 3. Send messages
-   * 
-   * data: {
-   *  users?: {id: string, email: string...},
-   *  message?: string,
-   *  owner_id?: string
-   * }
-   */
   ws.on("message", (message) => {
-    const data = JSON.parse(message);
-    if (!owner_id && !isUserExist(data.owner_id)) {
-      console.log("ERROR: no owner_id");
-      return;
-    } else if (isUserExist(data.owner_id) && !owner_id) {
-      owner_id = data.owner_id;
-      ws.send(JSON.stringify({ users: USERS }));
-      return;
-    }
-
-    sendMessages(data.users, data.message);
+    sendMessage(data.sender.id, data.receiver_id, data.message);
   });
 });
