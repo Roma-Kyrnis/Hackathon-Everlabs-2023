26e8ba294eac6a997a6097fbd9ba48d79cc823ad
Andry Zubov <alex470158@gmail.com>
2023-11-05 02:39:22 +0200

    Added tagging

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,10 @@ import WebSocket, { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 3000 });
 
 wss.on("connection", function connection(ws) {
+  ws.send(JSON.stringify([
+    { id: "Dev", name: "Developers" },
+    { id: "Ceo", name: "CEO" },
+  ]))
   ws.on("message", function message(message) {
     const data = JSON.parse(message);
     if (data.type === "message") {
