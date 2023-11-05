f5b282994e30e60be4a1826f160652d0bd102c52
Andry Zubov <alex470158@gmail.com>
2023-11-05 04:04:36 +0200

    update chat.js

--- Backend/server.js
+++ Backend/server.js
@@ -4,9 +4,23 @@ const wss = new WebSocketServer({ port: 3000 });
 
 wss.on("connection", function connection(ws) {
   ws.send(JSON.stringify([
-    { id: "Dev", name: "Developers" },
-    { id: "Ceo", name: "CEO" },
+    {
+      "id": "Vasya",
+      "email": "vasya@gmail.com",
+      "username": "Vasya1"
+    },
+    {
+      "id": "Petro",
+      "email": "petro@gmail.com",
+      "username": "Petro1"
+    },
+    {
+      "id": "Olezha",
+      "email": "olezha@gmail.com",
+      "username": "Olezha1"
+    }
   ]))
+  
   ws.on("message", function message(message) {
     const data = JSON.parse(message);
     if (data.type === "message") {
